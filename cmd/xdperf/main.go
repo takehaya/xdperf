c75e0f3c45bcfc803f0a9508c4ee40bacc0f52b4
Takeru Hayasaka <hayatake396@gmail.com>
2025-10-11 19:48:07 +0000

    feat: add cli base code

--- cmd/xdperf/main.go
+++ cmd/xdperf/main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := newApp(version)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalf("%+v", err)
+	}
+}
+
+func newApp(version string) *cli.App {
+	app := cli.NewApp()
+	app.Name = "Xdperf"
+	app.Version = fmt.Sprintf("%s, %s, %s, %s", version, commit, date, builtBy)
+
+	app.Usage = ""
+
+	app.EnableBashCompletion = true
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "plugin, p",
+			Value: "simple",
+			Usage: "plugin file name",
+		},
+		cli.StringFlag{
+			Name:  "plugin-path, P",
+			Usage: "plugin path, default is /usr/local/lib/xdperf/plugins/",
+		},
+	}
+	app.Action = run
+	return app
+}
+
+func run(ctx *cli.Context) error {
+	return nil
 }
