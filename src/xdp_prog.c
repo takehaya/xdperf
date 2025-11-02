611d077ea69d17b2b2c6e7698b8a828a21a2d68b
Takeru Hayasaka <hayatake396@gmail.com>
2025-11-02 20:53:49 +0000

    chore: fmt c code

--- src/xdp_prog.c
+++ src/xdp_prog.c
@@ -1,74 +1,74 @@
 #include "xdp_prog.h"
 #include "xdpcap.h"
 
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/if_vlan.h>
 #include <stdbool.h>
 #include <string.h>
-#include <bpf/bpf_endian.h>
-#include <bpf/bpf_helpers.h>
 
 char _license[] SEC("license") = "GPL";
 
 SEC("xdp")
-int xdp_tx(struct xdp_md *ctx)
-{
-    void *data     = (void *)(long)ctx->data;
-    void *data_end = (void *)(long)ctx->data_end;
-    __u32 zero = 0;
+int xdp_tx(struct xdp_md *ctx) {
+  void *data = (void *)(long)ctx->data;
+  void *data_end = (void *)(long)ctx->data_end;
+  __u32 zero = 0;
 
-    __u32 *pidx = bpf_map_lookup_elem(&seq_state_map, &zero);
-    __u32 idx = pidx ? *pidx : 0;
-    if (idx >= MAX_PACKET_ENTRY)
-        idx = 0;
+  __u32 *pidx = bpf_map_lookup_elem(&seq_state_map, &zero);
+  __u32 idx = pidx ? *pidx : 0;
+  if (idx >= MAX_PACKET_ENTRY)
+    idx = 0;
 
-    struct pkt_template *pt = bpf_map_lookup_elem(&tx_override_map, &idx);
-    if (!pt)
-        return XDP_ABORTED;
+  struct pkt_template *pt = bpf_map_lookup_elem(&tx_override_map, &idx);
+  if (!pt)
+    return XDP_ABORTED;
 
-    __u32 tlen = pt->len;
-    if (tlen > MAX_TEMPLATE_SIZE)
-        tlen = MAX_TEMPLATE_SIZE;
+  __u32 tlen = pt->len;
+  if (tlen > MAX_TEMPLATE_SIZE)
+    tlen = MAX_TEMPLATE_SIZE;
 
-    __u32 cur_len = data_end - data;
-    if (cur_len != tlen) {
-        int delta = (int)tlen - (int)cur_len;
-        if (bpf_xdp_adjust_tail(ctx, delta) < 0)
-            return XDP_ABORTED;
-        data     = (void *)(long)ctx->data;
-        data_end = (void *)(long)ctx->data_end;
-        if (data + tlen > data_end)
-            return XDP_ABORTED;
-    }
-
-    // override payload
+  __u32 cur_len = data_end - data;
+  if (cur_len != tlen) {
+    int delta = (int)tlen - (int)cur_len;
+    if (bpf_xdp_adjust_tail(ctx, delta) < 0)
+      return XDP_ABORTED;
+    data = (void *)(long)ctx->data;
+    data_end = (void *)(long)ctx->data_end;
     if (data + tlen > data_end)
-        return XDP_ABORTED;
+      return XDP_ABORTED;
+  }
 
-    for (int i = 0; i < MAX_TEMPLATE_SIZE; i++) {
-        if (i >= (int)tlen)
-            break;
+  // override payload
+  if (data + tlen > data_end)
+    return XDP_ABORTED;
 
-        void *dp = data + i;
-        if (dp + 1 > data_end)
-            return XDP_ABORTED;
+  for (int i = 0; i < MAX_TEMPLATE_SIZE; i++) {
+    if (i >= (int)tlen)
+      break;
 
-        *(__u8 *)dp = pt->data[i];
-    }
+    void *dp = data + i;
+    if (dp + 1 > data_end)
+      return XDP_ABORTED;
 
-    // next index
-    if (pidx) {
-        __u32 next = idx + 1;
-        if (next >= MAX_PACKET_ENTRY) next = 0;
-        *pidx = next;
-    }
+    *(__u8 *)dp = pt->data[i];
+  }
 
-    // sended packet stats
-	struct datarec *rec = bpf_map_lookup_elem(&stats_map, &zero);
-	if (!rec)
-		return XDP_ABORTED;
-	rec->rx_packets++;
-	rec->rx_bytes += ctx->data_end - ctx->data;
-	return XDP_TX;
+  // next index
+  if (pidx) {
+    __u32 next = idx + 1;
+    if (next >= MAX_PACKET_ENTRY)
+      next = 0;
+    *pidx = next;
+  }
+
+  // sended packet stats
+  struct datarec *rec = bpf_map_lookup_elem(&stats_map, &zero);
+  if (!rec)
+    return XDP_ABORTED;
+  rec->rx_packets++;
+  rec->rx_bytes += ctx->data_end - ctx->data;
+  return XDP_TX;
 };
--- src/xdp_prog.h
+++ src/xdp_prog.h
@@ -1,41 +1,40 @@
 #ifndef XDP_UTILS_H
 #define XDP_UTILS_H
+#include <bpf/bpf_helpers.h>
 #include <linux/bpf.h>
 #include <linux/in.h>
-#include <bpf/bpf_helpers.h>
 
 struct datarec {
-	__u64 rx_packets;
-	__u64 rx_bytes;
+  __u64 rx_packets;
+  __u64 rx_bytes;
 };
 
 struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__type(key, __u32);
-	__type(value, struct datarec);
-	__uint(max_entries, 1);
+  __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+  __type(key, __u32);
+  __type(value, struct datarec);
+  __uint(max_entries, 1);
 } stats_map SEC(".maps");
 
-
 #define MAX_PACKET_ENTRY 2048
-#define MAX_TEMPLATE_SIZE  2048
+#define MAX_TEMPLATE_SIZE 2048
 struct pkt_template {
-    __u32 len; // actual length of data
-    __u8  data[MAX_TEMPLATE_SIZE]; // raw frame
+  __u32 len;                    // actual length of data
+  __u8 data[MAX_TEMPLATE_SIZE]; // raw frame
 };
 struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __uint(max_entries, MAX_PACKET_ENTRY);
-    __type(key, __u32); // per-cpu id
-    __type(value, struct pkt_template);
+  __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+  __uint(max_entries, MAX_PACKET_ENTRY);
+  __type(key, __u32); // per-cpu id
+  __type(value, struct pkt_template);
 } tx_override_map SEC(".maps");
 
 // random or sequential per-cpu state
 struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __uint(max_entries, 1);
-    __type(key, __u32);
-    __type(value, __u32);
+  __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+  __uint(max_entries, 1);
+  __type(key, __u32);
+  __type(value, __u32);
 } seq_state_map SEC(".maps");
 
 #endif // XDP_UTILS_H
