02a9c31e3be6138288dfa35b1351c48b0d73ee21
Takeru Hayasaka <hayatake396@gmail.com>
2025-11-26 00:22:35 +0900

    refactor: add clang-fmt conf and fmt

--- src/xdpcap.h
+++ src/xdpcap.h
@@ -9,11 +9,10 @@
  * For example:
  *   struct bpf_map_def xdpcap_hook = XDPCAP_HOOK();
  */
-#define XDPCAP_HOOK()                                                          \
-  {                                                                            \
-    .type = BPF_MAP_TYPE_PROG_ARRAY, .key_size = sizeof(int),                  \
-    .value_size = sizeof(int), .max_entries = 5,                               \
-  }
+#define XDPCAP_HOOK()                                                                                                              \
+    {                                                                                                                              \
+        .type = BPF_MAP_TYPE_PROG_ARRAY, .key_size = sizeof(int), .value_size = sizeof(int), .max_entries = 5,                     \
+    }
 
 /**
  * Return action, exposing the action and input packet to xdpcap hook.
@@ -24,13 +23,14 @@
  *
  *   return XDP_PASS;
  */
-__attribute__((__always_inline__)) static inline enum xdp_action
-xdpcap_exit(struct xdp_md *ctx, void *hook_map, enum xdp_action action) {
-  // tail_call
-  // Some headers define tail_call (Cilium), others bpf_tail_call (kernel self
-  // tests). Use the helper ID directly
-  ((int (*)(struct xdp_md *, void *, int))12)(ctx, hook_map, action);
-  return action;
+__attribute__((__always_inline__)) static inline enum xdp_action xdpcap_exit(struct xdp_md *ctx, void *hook_map,
+                                                                             enum xdp_action action)
+{
+    // tail_call
+    // Some headers define tail_call (Cilium), others bpf_tail_call (kernel self
+    // tests). Use the helper ID directly
+    ((int (*)(struct xdp_md *, void *, int))12)(ctx, hook_map, action);
+    return action;
 }
 
 #endif /* XDPCAP_HOOK_H */
