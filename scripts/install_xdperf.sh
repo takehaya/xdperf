253e8a89613c1e5cad30e57f3f8e214432102c7e
Takeru Hayasaka <hayatake396@gmail.com>
2025-11-30 11:21:18 +0900

    chore: modify install script

--- scripts/install_xdperf.sh
+++ scripts/install_xdperf.sh
@@ -33,8 +33,12 @@ BIN_URL="$(echo "$JSON" | jq -r --arg a "$ARCH" '
 
 mapfile -t WASM_URLS < <(echo "$JSON" | jq -r --arg a "$ARCH" '
   .assets[]?.browser_download_url
-  | select(test("/.*\\.wasm_.*_linux_" + $a + "$"))
+  | select(
+      test("/.*\\.wasm_.*_linux_" + $a + "$")
+      or test("/.*_wasip1_wasm\\.wasm$")
+    )
 ')
+
 TAG_NAME="$(echo "$JSON" | jq -r '.tag_name // empty')"
 
 TMP="$(mktemp -d)"; trap 'rm -rf "$TMP"' EXIT
@@ -48,14 +52,21 @@ curl -fsSL "$BIN_URL" -o "$TMP/$BIN_NAME"
 chmod +x "$TMP/$BIN_NAME"
 install -m 0755 "$TMP/$BIN_NAME" "$BIN_DIR/xdperf"
 
-# プラグイン配置（同名のみ上書き）
+# put plugins
 for U in "${WASM_URLS[@]}"; do
   W="$(basename "$U")"
   curl -fsSL "$U" -o "$TMP/$W"
-  DEST_NAME="${W%%.wasm*}.wasm"
+  # case1: foo.wasm_version_linux_arch -> foo.wasm
+  # case2: foo_version_wasip1_wasm.wasm -> foo.wasm
+  if [[ "$W" =~ ^(.+)\.wasm_.+ ]]; then
+    DEST_NAME="${BASH_REMATCH[1]}.wasm"
+  elif [[ "$W" =~ ^(.+)_[0-9]+\.[0-9]+\.[0-9]+_wasip1_wasm\.wasm$ ]]; then
+    DEST_NAME="${BASH_REMATCH[1]}.wasm"
+  else
+    DEST_NAME="$W"
+  fi
   install -m 0644 "$TMP/$W" "$PLUGIN_DIR/$DEST_NAME"
 done
-
 echo "${TAG_NAME:-${VERSION:-latest}}" > "$STATE_FILE"
 
 echo "Installed xdperf"
